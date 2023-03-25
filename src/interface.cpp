70b18d82960deea5491e6885aa88c5c82c46f27b
progof <34421475+progof@users.noreply.github.com>
2023-03-25 21:23:46 +0100

    Added new program "mult_big_num"

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,43 +1,45 @@
 // OrtOS - Private manager for working with simple programs
 // Developmet to start 03/03/2023
 #include "interface.hpp"
-
-
+using namespace std;
 
 int main()
 {
-    system("clear");
-    std::cout << "Welcome to OrtOS manager [" << version << "]" << std::endl;
-    short option;
-
-    while (true)
-    {
-   
-        std::cout << "\nList of available programs:\n\n0. Exit\n1. Education: Guess X\n2. Finance: Compound Interest\n3. Education:  mult_big_num" << std::endl;
-        std::cout << "\n[OrtOS] Choose what you want to do: ";
-        std::cin >> option;
-
+    int option;
+    string confirm;
+    do
+    { 
+        system("clear");
+        cout << "\t\tWelcome to OrtOS manager [" << version << "]" << endl;
+        cout << "\nThis is a personal manager for working with simple programs,\nto run working with the manager, enter '0'" << endl;
+        cout<<"\n[OrtOS] Type id of program to run it $ ";
+        cin>>option;
+        
         switch (option)
         {
-        case 0:
-            break;   
-        case 1:
-            guess_x();
-            continue;
-        case 2:
-            compound_interest();
-            continue;
-        case 3:
-            mult_big_num();
-            continue; 
-        default:
-            std::cout << "\n\nError: No such option!!!\n\n" << std::endl;
-            continue;
+            case 0: 
+                displaymenu(); 
+                break;
+            case 1: 
+                info();
+                break;
+            case 2: 
+                guess_x();
+                break;
+            case 3: 
+                compound_interest(); 
+                break;
+            case 4: 
+                mult_big_num(); 
+                break;
+            default: 
+                cout << "\n\tInvalid program ID\n" << endl;
+                break;
         }
+        cout<<"Press 'y' or 'Y' to continue: ";
+        cin>>confirm;
 
-        if (option == 0) exit(0);
-        
-    }
-
-    return 0;
+    } while (confirm == "y" || confirm == "Y");
+    
+    return EXIT_SUCCESS;
 }
--- src/interface.hpp
+++ src/interface.hpp
@@ -4,11 +4,32 @@
 #define INTERFACE_H
 
 #include <iostream>
+#include <string>
 #include "programs/education/guess_x.hpp"
 #include "programs/education/mult_big_num.hpp" 
 #include "programs/education/compress.hpp" 
 #include "programs/finance/compound_interest.hpp"
 
-char version[] = "1.3.1";
+using namespace std;
+char version[] = "2.3.1";
+
+void displaymenu()
+{ 
+    string programs_list[] = {"Menu", "Info", "Guess X", "Compound Interest", "Multiplication of large numbers"};
+    std::cout << "\n[ID] Programs name:" << std::endl;
+        for (int i = 0; i < 4; i++)
+        {
+            std::cout << "[" << i << "] " << programs_list[i] << std::endl;
+        }
+        cout << endl;
+}
+
+void info()
+{
+    cout << "\n'Guess X' => ..." << endl;
+    cout << "\n'Compound Interest' => ..." << endl;
+    cout << "\n'Multiplication of large numbers' => ..." << endl;
+    cout << endl;
+}
 
 #endif
